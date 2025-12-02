d67ac540e399749f33359b785502887dd99429d1
zuzanna-z <131006601+zuzanna-z@users.noreply.github.com>
2025-12-03 00:54:57 +0100

    Add files via upload

---
+++ assets/data_control_dk.js
@@ -0,0 +1,352 @@
+const control_set_dk = [
+  {
+    "word": "dictionary",
+    "translation": "ordbog",
+    "wrong_1": "l\u00e6rebog",
+    "wrong_2": "bog",
+    "wrong_3": "atlas"
+  },
+  {
+    "word": "work",
+    "translation": "arbejde",
+    "wrong_1": "erhverv",
+    "wrong_2": "m\u00e5l",
+    "wrong_3": "mission"
+  },
+  {
+    "word": "exercise",
+    "translation": "opgave",
+    "wrong_1": "undervisning",
+    "wrong_2": "foredrag",
+    "wrong_3": "hjemmearbejde"
+  },
+  {
+    "word": "happy",
+    "translation": "lykkelig",
+    "wrong_1": "ansvarlig",
+    "wrong_2": "d\u00e5rlig",
+    "wrong_3": "sjov"
+  },
+  {
+    "word": "wireless",
+    "translation": "tr\u00e5dl\u00f8s",
+    "wrong_1": "kablet",
+    "wrong_2": "elektrisk",
+    "wrong_3": "fjernstyret"
+  },
+  {
+    "word": "food",
+    "translation": "mad",
+    "wrong_1": "kok",
+    "wrong_2": "dessert",
+    "wrong_3": "tallerken"
+  },
+  {
+    "word": "game",
+    "translation": "spil",
+    "wrong_1": "spiller",
+    "wrong_2": "udf\u00f8relse",
+    "wrong_3": "konkurrence"
+  },
+  {
+    "word": "source",
+    "translation": "udspring",
+    "wrong_1": "br\u00f8nd ",
+    "wrong_2": "bevis",
+    "wrong_3": "kilde"
+  },
+  {
+    "word": "treatment",
+    "translation": "behandling",
+    "wrong_1": "projekt",
+    "wrong_2": "plan",
+    "wrong_3": "pr\u00f8ve"
+  },
+  {
+    "word": "order",
+    "translation": "bestilling",
+    "wrong_1": "levering",
+    "wrong_2": "postv\u00e6sen",
+    "wrong_3": "forsendelse "
+  },
+  {
+    "word": "old",
+    "translation": "gammel",
+    "wrong_1": "beskadiget",
+    "wrong_2": "ensom",
+    "wrong_3": "sentimental"
+  },
+  {
+    "word": "delivery",
+    "translation": "levering",
+    "wrong_1": "bestilling",
+    "wrong_2": "postv\u00e6sen",
+    "wrong_3": "forsendelse "
+  },
+  {
+    "word": "love",
+    "translation": "k\u00e6rlighed",
+    "wrong_1": "lykke",
+    "wrong_2": "sundhed",
+    "wrong_3": "forhold"
+  },
+  {
+    "word": "yellow",
+    "translation": "gul",
+    "wrong_1": "r\u00f8d",
+    "wrong_2": "orange",
+    "wrong_3": "gr\u00f8n"
+  },
+  {
+    "word": "amount",
+    "translation": "m\u00e6ngde",
+    "wrong_1": "st\u00f8rrelse",
+    "wrong_2": "beskrivelse",
+    "wrong_3": "v\u00e6gt"
+  },
+  {
+    "word": "subject",
+    "translation": "emne",
+    "wrong_1": "forel\u00e6sning ",
+    "wrong_2": "l\u00e6rer",
+    "wrong_3": "undervisning"
+  },
+  {
+    "word": "fire",
+    "translation": "brand",
+    "wrong_1": "olie",
+    "wrong_2": "gas",
+    "wrong_3": "r\u00f8g"
+  },
+  {
+    "word": "everyone",
+    "translation": "alle",
+    "wrong_1": "nogen",
+    "wrong_2": "ingen",
+    "wrong_3": "os"
+  },
+  {
+    "word": "battle",
+    "translation": "kamp",
+    "wrong_1": "v\u00e5ben",
+    "wrong_2": "fred",
+    "wrong_3": "slagmark"
+  },
+  {
+    "word": "experience",
+    "translation": "oplevelse",
+    "wrong_1": "eventyr",
+    "wrong_2": "rejse",
+    "wrong_3": "tur"
+  },
+  {
+    "word": "event",
+    "translation": "begivenhed",
+    "wrong_1": "forsamling",
+    "wrong_2": "fest",
+    "wrong_3": "eventyr"
+  },
+  {
+    "word": "need",
+    "translation": "behov",
+    "wrong_1": "valg",
+    "wrong_2": "opfattelse",
+    "wrong_3": "id\u00e9"
+  },
+  {
+    "word": "island",
+    "translation": "\u00f8",
+    "wrong_1": "jord",
+    "wrong_2": "land",
+    "wrong_3": "kyst"
+  },
+  {
+    "word": "pocket",
+    "translation": "lomme",
+    "wrong_1": "lynl\u00e5s ",
+    "wrong_2": " h\u00e5ndtaske",
+    "wrong_3": "pengepung"
+  },
+  {
+    "word": "animal",
+    "translation": "dyr.",
+    "wrong_1": " k\u00e6ledyr",
+    "wrong_2": "ven",
+    "wrong_3": "vild"
+  },
+  {
+    "word": "shape",
+    "translation": "form",
+    "wrong_1": "terning",
+    "wrong_2": "trekant",
+    "wrong_3": "vinkel"
+  },
+  {
+    "word": "dark",
+    "translation": "m\u00f8rk",
+    "wrong_1": "bl\u00f8d",
+    "wrong_2": "sort",
+    "wrong_3": "rolig"
+  },
+  {
+    "word": "movement",
+    "translation": "bev\u00e6gelse",
+    "wrong_1": "film",
+    "wrong_2": "overfart ",
+    "wrong_3": "f\u00e6rdsel"
+  },
+  {
+    "word": "skin",
+    "translation": "hud",
+    "wrong_1": "membrane",
+    "wrong_2": "overflade",
+    "wrong_3": "l\u00e6der "
+  },
+  {
+    "word": "player",
+    "translation": "spiller",
+    "wrong_1": "tilskuer",
+    "wrong_2": "tr\u00e6ner ",
+    "wrong_3": "d\u00f8mmer"
+  },
+  {
+    "word": "engine",
+    "translation": "motor",
+    "wrong_1": "bil",
+    "wrong_2": "tog",
+    "wrong_3": "turbine"
+  },
+  {
+    "word": "money",
+    "translation": "penge",
+    "wrong_1": "rigdom",
+    "wrong_2": "m\u00f8nt",
+    "wrong_3": "pengeseddel"
+  },
+  {
+    "word": "road",
+    "translation": "vej",
+    "wrong_1": "gangsti",
+    "wrong_2": "indgang",
+    "wrong_3": "motorvej"
+  },
+  {
+    "word": "today",
+    "translation": "i dag",
+    "wrong_1": "i morgen",
+    "wrong_2": "aldrig",
+    "wrong_3": "om aftenen"
+  },
+  {
+    "word": "royal",
+    "translation": "kongelig",
+    "wrong_1": "vigtig",
+    "wrong_2": "dyr.",
+    "wrong_3": "overd\u00e5dig"
+  },
+  {
+    "word": "memory",
+    "translation": "minde",
+    "wrong_1": "k\u00e6rlighed ",
+    "wrong_2": "b\u00e5nd",
+    "wrong_3": "f\u00f8lelse "
+  },
+  {
+    "word": "error",
+    "translation": "fejl",
+    "wrong_1": "adgang",
+    "wrong_2": "mangel",
+    "wrong_3": "l\u00f8gn"
+  },
+  {
+    "word": "friend",
+    "translation": "ven",
+    "wrong_1": "familie",
+    "wrong_2": "bekendtskab",
+    "wrong_3": "kollega"
+  },
+  {
+    "word": "close",
+    "translation": "t\u00e6t",
+    "wrong_1": "fjern",
+    "wrong_2": "lav",
+    "wrong_3": "h\u00f8j"
+  },
+  {
+    "word": "secret",
+    "translation": "hemmelig",
+    "wrong_1": "rygte",
+    "wrong_2": "forbandelse",
+    "wrong_3": "tale"
+  },
+  {
+    "word": "border",
+    "translation": "gr\u00e6nse",
+    "wrong_1": "land",
+    "wrong_2": "hegn",
+    "wrong_3": "v\u00e6g"
+  },
+  {
+    "word": "belief",
+    "translation": "tro",
+    "wrong_1": "m\u00e5l",
+    "wrong_2": "liv",
+    "wrong_3": "mission"
+  },
+  {
+    "word": "share",
+    "translation": "dele",
+    "wrong_1": "give",
+    "wrong_2": "sende",
+    "wrong_3": "droppe"
+  },
+  {
+    "word": "hot",
+    "translation": "varmt",
+    "wrong_1": "lunken",
+    "wrong_2": "sommer",
+    "wrong_3": "fugtig"
+  },
+  {
+    "word": "stock",
+    "translation": "lager",
+    "wrong_1": "straf",
+    "wrong_2": "salg",
+    "wrong_3": "varer"
+  },
+  {
+    "word": "picture",
+    "translation": "billede",
+    "wrong_1": "kunst",
+    "wrong_2": "plakat ",
+    "wrong_3": "bil"
+  },
+  {
+    "word": "responsible",
+    "translation": "ansvarlig",
+    "wrong_1": "vigtig",
+    "wrong_2": "elegant",
+    "wrong_3": "intelligent "
+  },
+  {
+    "word": "car",
+    "translation": "bil",
+    "wrong_1": "tog",
+    "wrong_2": "bus",
+    "wrong_3": "cykel"
+  },
+  {
+    "word": "poor",
+    "translation": "fattig",
+    "wrong_1": "syg",
+    "wrong_2": "ulykkelig ",
+    "wrong_3": "ensome"
+  },
+  {
+    "word": "channel",
+    "translation": "kanal",
+    "wrong_1": "flod",
+    "wrong_2": "str\u00f8m",
+    "wrong_3": "udsendelse "
+  }
+]
---
+++ assets/text_data.js
@@ -0,0 +1,19 @@
+const info_agree_1 = 'Hello! You are about to participate in my research project. Participating in this experiment will not provide you with any direct benefits, but you will have the opportunity to help me with my bachelor project. All the data is collected solely for research purposes. However, I ask that you refrain from sharing personal or sensitive information when completing this experiment. Press space to continue.'
+
+const info_agree_2 = 'Participation in the study is voluntary, and you may withdraw your participation consent without penalty at any time during the experiment. After the experiment is completed, you will not be able to withdraw your consent, as the data will be anonymized, making it impossible to identify the participant. The experiment will take approximately 10 minutes to complete. If you are ready to begin, press space.'
+
+
+const intro = 'Welcome to the experiment! If you are ready to begin, press space.'
+
+const instructions_trial = 'You are about to start a practice trial. Here you will be presented with a shape. Your task is to decide whether the shape on the screen is a triangle or a circle. You must press the "w" key on your keyboard, if the shape is a triangle or the "n" key  if the shape is a circle. Press "space" to begin.'
+
+const instructions_polish = 'You have finished the practice task. You will now repeat the experiment on words instead of shapes. This time you will be presented with a word and must decide whether it is a real word or a nonsense word (i.e. string of letters with no meaning behind it). You must press the "w" key on your keyboard for a real word or the "n" key for a nonsense word. Press space to begin.'
+
+const instructions_english = 'You have now finished the first test stage of this experiment. You will now repeat the same task. Once again you will be presented with a word, you must decide whether it is a real word or a nonsense word. You must press the "w" key for a real word or the "n" key for a nonsense word. Press space to begin.'
+
+const instructions_control = 'This is the last stage of the experiment! This stage tests your knowledge of the English words you were presented with in the previous stage. Each word will be displayed with 4 possible translations. To answer simply click on the option of your choosing. Remember that it is NOT possible to change your answer once you check one of the boxes.'
+
+const outro = 'You have completed the experiment. I will now ask you to download and upload your data to a form linked below. The button displayed underneath will download a .csv file to your local download folder (the file name should be formated as [your_id]_data.csv). To upload the data simply open the file you just downloaded, select all and copy (Windows: press ctrl + a and then ctrl + c | Macbook: command + a and then command + c) then open the form link and paste it in the response window. Thank you for your  participation!'
+
+const end_form_instructions = 'I will now ask you to fill out a short form. The information you share will be completely anonymized and used only for the purposes of this study.'
+
